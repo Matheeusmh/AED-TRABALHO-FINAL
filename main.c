4837642f9c6651992b013ebe2cb2076b68925cdb
Matheus Henrique <matheushenrique.mh080@gmail.com>
2024-11-07 13:09:36 -0300

    add

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ typedef struct noCidades {
 
 typedef struct noRodovias{
     char rodovia[8];
-    NoCidades *cidades;
+    descritorCidades *cidades;
     struct noRodovias *prox;
 } NoRodovias;
 
@@ -22,19 +22,75 @@ typedef struct {
     int quantRodovias;
 } descritorRodovias;
 
+typedef struct {
+    NoCidades *inicio;
+    NoCidades *final;
+    int quantCidades;
+} descritorCidades;
+
 typedef NoCidades *novaCidade;
 typedef NoRodovias *novaRodovia;
 
-NoCidades *iniciarCidades() {
-    NoCidades *novo;
-    novo = (NoCidades *)malloc(sizeof(NoCidades));
+char *stringToUpr(char *stringOrig) {
+    char *stringCpy;
+    stringCpy = (char *)malloc((strlen(stringOrig) + 1) * sizeof(char));
+    if(stringCpy == NULL) {
+        printf("Erro ao alocar memoria!\n");
+        return NULL;
+    }
+
+    strcpy(stringCpy, stringOrig);
+
+    for(int i = 0; stringCpy[i] != '\0'; i++) {
+        stringCpy[i] = toupper(stringCpy[i]);
+    }
+
+    return stringCpy;
+}
+
+descritorCidades *iniciarCidades() {
+    descritorCidades *novo;
+    novo = (descritorCidades *)malloc(sizeof(descritorCidades));
     if(novo == NULL) {
         printf("Erro ao alocar memoria!\n");
         return NULL;
     }
+
+    novo->inicio = NULL;
+    novo->final = NULL;
+
+    return novo;
 }
 
-void adicionarCidade(NoRodovias *ptrRodovia, char *cidade, char *estado, float km) {
+descritorRodovias *iniciarRodovias() {
+    descritorRodovias *novo;
+    novo = (descritorRodovias *)malloc(sizeof(descritorRodovias));
+    if(novo == NULL) {
+        printf("Erro ao alocar memoria!\n");
+        return NULL;
+    }
+
+    novo->inicio = NULL;
+    novo->final = NULL;
+    novo->quantRodovias = 0;
+
+    return novo;
+}
+
+int verificadorCidade(descritorCidades *cidades, char *cidade) {
+    novaCidade aux = cidades->inicio;
+
+    while(aux != NULL) {
+        if(strcmp(stringToUpr(aux->cidade), stringToUpr(cidade)) == 0) return 1;
+    }
+
+    return 0;
+}
+
+void adicionarCidade(descritorCidades *cidades, char *cidade, char *estado, float km) {
+    if(verificadorCidade(cidades, cidade)) {
+        return;
+    }
     novaCidade addCidade;
     addCidade = (NoCidades *)malloc(sizeof(NoCidades));
     if(addCidade == NULL) {
@@ -45,22 +101,55 @@ void adicionarCidade(NoRodovias *ptrRodovia, char *cidade, char *estado, float k
     strcpy(addCidade->cidade, cidade);
     strcpy(addCidade->estado, estado);
     addCidade->km = km;
+    addCidade->prox = NULL;
+    addCidade->ant = NULL;
 
-    novaCidade auxCidade = (*ptrRodovia)->cidades;
+    if(cidades->inicio == NULL) {
+        cidades->inicio = addCidade;
+        cidades->final = addCidade;
+    }
+    else {
+        novaCidade aux = cidades->inicio;
 
-    while(auxCidade->km <= addCidade->km) {
-        auxCidade = auxCidade->prox;
+        while(km >= aux->km && aux != NULL) {
+            aux = aux->prox;
+        }
+
+        addCidade->prox = aux;
+        aux->ant->prox = addCidade;
+        addCidade->ant = aux->ant;
+        aux->ant = addCidade;
+
+        if(aux == NULL) {
+            cidades->final = addCidade;
+        }
+        else if(aux->ant = NULL) {
+            cidades->inicio = addCidade;
+        }
     }
 
-    auxCidade->ant->prox = addCidade;
-    addCidade->ant = auxCidade->ant;
+    cidades->quantCidades++;
+}
 
-    addCidade->prox = auxCidade;
-    auxCidade->ant = addCidade;
+descritorCidades *verificadorRodovia(descritorRodovias *descritor, char *rodovia) {
+    novaRodovia aux = descritor->inicio;
+    while(aux != NULL) {
+        if(strcmp(stringToUpr(aux->rodovia), stringToUpr(rodovia)) == 0) aux->cidades;
+    }
 
+    return NULL;
 }
 
 void adicionarRodovia(descritorRodovias *descritor, char *rodovia, char *cidade, char *estado, float km) {
+    if(verificadorRodovia(descritor, rodovia) != NULL) {
+        adicionarCidade(verificadorRodovia(descritor, rodovia), cidade, estado, km);
+        return;
+    }
+
+    novaRodovia aux = descritor->inicio;
+    while(aux != NULL) {
+
+    }
     novaRodovia nova;
     nova = (NoRodovias *)malloc(sizeof(NoRodovias));
     if(nova == NULL) {
@@ -70,26 +159,19 @@ void adicionarRodovia(descritorRodovias *descritor, char *rodovia, char *cidade,
 
     strcpy(nova->rodovia, rodovia);
     nova->cidades = iniciarCidades();
+    adicionarCidade(nova->cidades, cidade, estado, km);
+    nova->prox = NULL;
 
-    while(auxRodovia != NULL) {
-        if(strcmp(auxRodovia->rodovia, rodovia) == 0) {
-            adicionarCidade(&auxRodovia, rodovia, cidade, estado, km);
-            return;
-        }
+    if(descritor->inicio == NULL) {
+        descritor->inicio =  nova;
+        descritor->final = nova;
+    }
+    else {
+        descritor->final->prox = nova;
+        descritor->final = nova;
     }
 
-    novaRodovia addRodovia;
-    addRodovia = (NoRodovias *)malloc(sizeof(NoRodovias));
-    if(addRodovia == NULL) {
-        printf("Erro ao alocar memoria!\n");
-        return;
-    }
-
-    (*descritor)->final->prox = addRodovia;
-    (*descritor)->final = addRodovia;
-
-    iniciarRodovia(descritor, rodovia, cidade, estado, km);
-
+    descritor->quantRodovias++;
 }
 
 
